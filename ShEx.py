26eb69e6f3370b33110ffad65c0b9b33ee432115
andrawaag <andra@waagmeester.net>
2017-09-18 16:37:25 +0200

    Check if validate is installed

--- ShEx.py
+++ ShEx.py
@@ -1,3 +1,4 @@
+import shutil
 import shlex
 import json
 import subprocess
@@ -6,15 +7,19 @@ import pprint
 
 class validate:
     def __init__(self, shex, data):
+        if shutil.which("validate") == None:
+            i = subprocess.check_output("npm install -g shex")
         self.data = data
         self.shex = shex
         self.output = "{}"
         args = shlex.split(
-            "/Users/andra/projects/shex.js/bin/validate -x {} -d {}".format(self.shex, self.data))
+            "validate -x {} -d {}".format(self.shex, self.data))
         try:
             p = subprocess.check_output(args)
         except subprocess.CalledProcessError as grepexc:
             self.output = json.loads(grepexc.output)
+
+
 
 def main():
     input_url = "https://www.wikidata.org/wiki/Special:EntityData/Q42"
